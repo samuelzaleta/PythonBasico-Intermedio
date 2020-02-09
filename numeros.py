7daf8b67a74bed62ba38f3cb9dc228b885233a8e
Samuel Zaleta <samuel.zaleta.cbtis103@gmail.com>
2020-02-08 19:40:10 -0600

    ejercicios basicos de numero

---
+++ numeros.py
@@ -0,0 +1,18 @@
+'''*
+** Numeros
+'''
+'''interprete como calculadora'''
+print( 2 + 2) # operador de suma es +
+print((50 - 5*6)/4) # operador de multipliación * , operador de resta -
+print(17/3) #operador de división /
+print(17//3) #trucar residuo de división
+print(17 % 3) #Operación módulo que regresa el residuo de realizar una división entera entre dos números %
+print(2 ** 9) #operador de potencia **
+width = 20
+height = 5 * 9
+print(width * height)
+print( 4 * 3.75 -1) #Hay soporte completo para coma flotante; Los operadores con operandos de tipo mixto convierten el operando entero en coma flotante
+tax = 12.5/100
+price = 100
+print(price*tax)
+
